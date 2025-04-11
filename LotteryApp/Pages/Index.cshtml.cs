dcae191b39372056b58ccd074a407517d64e090d
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-04-11 10:33:04 +0100

    feat: enhance Index page to display main numbers and bonus ball with styling

--- LotteryApp/Pages/Index.cshtml.cs
+++ LotteryApp/Pages/Index.cshtml.cs
@@ -1,20 +1,29 @@
 using LotteryApp.Contracts.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-public class IndexModel : PageModel {
+public class IndexModel : PageModel
+{
     private readonly ILotteryNumberGenerator _lotteryNumberGenerator;
-    public List<int> MainNumbers { get; set; }
-public int BonusBall { get; set; }
+    public List<int> MainNumbers { get; set; } = new List<int>();
+    public int BonusBall { get; set; }
 
-    public IndexModel(ILotteryNumberGenerator lotteryNumberGenerator) {
+    public IndexModel(ILotteryNumberGenerator lotteryNumberGenerator)
+    {
         _lotteryNumberGenerator = lotteryNumberGenerator;
     }
-    public void OnGet() {
+    public void OnGet()
+    {
         var mainNumbers = _lotteryNumberGenerator.GenerateUniqueNumbers();
         var sortedNumbers = _lotteryNumberGenerator.SortNumbers(mainNumbers.ToList());
         var bonusBall = _lotteryNumberGenerator.GenerateBonusBall(sortedNumbers);
 
         MainNumbers = sortedNumbers;
-        bonusBall = BonusBall;
+        BonusBall = bonusBall;
     }
+
+    public string GetColorClass(int number)
+    {
+        return _lotteryNumberGenerator.GetColorClass(number);
+    }
+
 }
